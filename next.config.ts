b649ea4ce469b5907da81f58d3ddcd5d7cebb8a0
subvrik-sys32 <joel.sebastian.aguilar.huaman@gmail.com>
2026-07-05 08:13:26 -0500

    feat: realtime engine multiuser

--- next.config.ts
+++ next.config.ts
@@ -3,12 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
-  experimental: {
-    staleTimes: {
-      dynamic: 180,
-      static: 300,
-    },
-  },
 };
 
 export default nextConfig;

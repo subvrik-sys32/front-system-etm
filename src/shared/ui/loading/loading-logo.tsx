2f73e6c7f55d7618fd2120ddb6a1616f10d886ac
subvrik-sys32 <joel.sebastian.aguilar.huaman@gmail.com>
2026-07-05 23:03:27 -0500

    chore: full production deployment

--- src/shared/ui/loading/loading-logo.tsx
+++ src/shared/ui/loading/loading-logo.tsx
@@ -11,27 +11,23 @@ export const LoadingLogo = memo(function LoadingLogo({
   logo,
 }: Props) {
 
-  if (logo) {
-    return (
-      <div className="flex h-18 w-18 items-center justify-center">
-        {logo}
-      </div>
-    )
-  }
-
   return (
 
     <div className="flex h-18 w-18 items-center justify-center">
 
-      <Image
-        src="/icon.svg"
-        alt="ETM SAC"
-        width={42}
-        height={42}
-        priority
-        draggable={false}
-        className="block h-8 w-auto select-none"
-      />
+      {logo ?? (
+
+        <Image
+          src="/icon.svg"
+          alt="ETM SAC"
+          width={42}
+          height={42}
+          priority
+          draggable={false}
+          className="block h-8 w-auto select-none"
+        />
+
+      )}
 
     </div>
 

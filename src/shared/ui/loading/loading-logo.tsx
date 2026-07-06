8a6ea682355cbfa88e497594cf01555c25ac662a
subvrik-sys32 <joel.sebastian.aguilar.huaman@gmail.com>
2026-07-06 12:39:02 -0500

    feat: realtime engine multiuser

--- src/shared/ui/loading/loading-logo.tsx
+++ src/shared/ui/loading/loading-logo.tsx
@@ -14,7 +14,7 @@ export const LoadingLogo=
 
     return(
 
-      <div className="flex h-28 w-full items-center justify-center px-6 pt-9">
+      <div className="flex h-28 w-full items-center justify-center">
 
         {logo??(
 

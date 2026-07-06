75cbe1ac7796cf758759f2c50db877370bd9eb73
subvrik-sys32 <joel.sebastian.aguilar.huaman@gmail.com>
2026-07-06 12:37:55 -0500

    feat: realtime engine multiuser

--- src/shared/ui/loading/loading-logo.tsx
+++ src/shared/ui/loading/loading-logo.tsx
@@ -3,33 +3,35 @@
 import { memo } from "react"
 import Image from "next/image"
 
-type Props = {
-  logo?: React.ReactNode
+type Props={
+  logo?:React.ReactNode
 }
 
-export const LoadingLogo = memo(function LoadingLogo({
-  logo,
-}: Props) {
+export const LoadingLogo=
+  memo(function LoadingLogo({
+    logo,
+  }:Props){
 
-  return (
+    return(
 
-    <div className="flex items-center justify-center px-6 pt-9">
+      <div className="flex h-28 w-full items-center justify-center px-6 pt-9">
 
-      {logo ?? (
+        {logo??(
 
-        <Image
-          src="/icon.svg"
-          alt="ETM SAC"
-          width={80}
-          height={80}
-          priority
-          className="block select-none"
-        />
+          <Image
+            src="/icon.svg"
+            alt="ETM SAC"
+            width={80}
+            height={80}
+            priority
+            draggable={false}
+            className="block h-20 w-20 select-none object-contain"
+          />
 
-      )}
+        )}
 
-    </div>
+      </div>
 
-  )
+    )
 
-})
+  })

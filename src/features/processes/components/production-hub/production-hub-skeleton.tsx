b83f752b54060c613f4a1fa13afd50e9546a13b6
subvrik-sys32 <joel.sebastian.aguilar.huaman@gmail.com>
2026-07-16 17:42:49 -0500

    chore: mobile tasks edit

--- src/features/processes/components/production-hub/production-hub-skeleton.tsx
+++ src/features/processes/components/production-hub/production-hub-skeleton.tsx
@@ -24,7 +24,7 @@ function SkeletonProcessRow({ opacity }: { opacity: number }) {
 
       <span className="h-11 w-11 shrink-0 rounded-xl bg-white/5" />
 
-      <span className="hidden h-4 w-20 shrink-0 rounded bg-white/10 tablet:block" />
+      <span className="h-4 w-20 shrink-0 rounded bg-white/10" />
 
       <div className="flex min-w-0 flex-1 items-center justify-end gap-4 tablet:gap-8">
 
@@ -36,7 +36,7 @@ function SkeletonProcessRow({ opacity }: { opacity: number }) {
         <span className="h-8 w-px shrink-0 bg-white/10" />
 
         <div className="text-right">
-          <span className="ml-auto block h-4 w-24 rounded bg-white/8" />
+          <span className="ml-auto block h-4 w-14 rounded bg-white/8" />
           <span className="ml-auto block h-6 w-6 rounded bg-white/12" />
         </div>
 
